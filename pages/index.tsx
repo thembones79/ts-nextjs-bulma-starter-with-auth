fe9b2ed3c06f0a482f7125203ec43e802093501b
thembones79 <michal.szumnarski@gmail.com>
2021-06-01 15:33:09 +0200

    added changelog and readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Landing = ({ currentUser, version }: LandingProps) => {
 };
 
 Landing.getInitialProps = ({ currentUser }: { currentUser: IUser }) => {
-  return { version: "0.0.4", currentUser };
+  return { version: "0.1.0", currentUser };
 };
 
 export default Landing;
